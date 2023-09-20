1b04b08af88499023cc6aad0c922ed3b53bbbb3c
陈云德 <chenyunde@dangdang.com>
2023-09-20 15:57:39 +0800

    添加跨域配置

--- main.go
+++ main.go
@@ -35,6 +35,7 @@ func main() {
 	defer db.Close()
 	initTree(db)
 	r := gin.Default()
+	r.Use(Core())
 	r.GET("/addnode", func(c *gin.Context) {
 		node := treeNode{
 			TreeNodeId:   2222,
@@ -58,6 +59,24 @@ func main() {
 		})
 	})
 	r.Run() // 监听并在 0.0.0.0:8080 上启动服务
+}
+
+// 解决跨域问题
+func Core() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		method := c.Request.Method
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Headers", "Content-Type,AccessToken,X-CSRF-Token,Authorization,Token")
+		c.Header("Access-Control-Allow-Methods", "POST,GET,OPTIONS")
+		c.Header("Access-Control-Expose-Headers", "Content-Length,Access-Control-Allow-Origin,Access-Control-Allow-Headers,Content-Type")
+		c.Header("Access-Control-Allow-Credentials", "True")
+		//放行索引options
+		if method == "OPTIONS" {
+			c.AbortWithStatus(http.StatusNoContent)
+		}
+		//处理请求
+		c.Next()
+	}
 }
 
 func initMysql() *sql.DB {
